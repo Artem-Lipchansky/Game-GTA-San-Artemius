a125e217e7f6cd3cfb43976f4d4048a089c2c485
Artem Lipchansky <lipchanskiy.an@gmail.com>
2023-05-03 15:35:06 +0300

    added

--- enemy.js
+++ enemy.js
@@ -13,5 +13,46 @@ export class Enemy{
             this.x = Math.random() * canvasWidth;
             this.y = Math.random() < 0.5 ? 0 - this.radius : canvasHeight + this.radius;
         }
+
+        this.image = new Image();
+        this.image.src = "./images/enemy-b.png";
+        this.imageWidth = 50;
+        this.imageHeight = 60;
+        this.imageTick = 0;
+
+    }
+
+    drawImg(){
+        const imageTickLimit = 18;
+        const subX = this.imageTick > imageTickLimit ? this.imageWidth : 0;
+        this.imageTick++;
+        if (this.imageTick > imageTickLimit * 2) this.imageTick = 0;
+
+
+        this.context.drawImage(
+            this.image,
+            subX,
+            0,
+            this.imageWidth,
+            this.imageHeight,
+            this.x - this.imageWidth/2,
+            this.y - this.imageHeight/2,
+            this.imageWidth,
+            this.imageHeight
+
+
+
+
+
+        );
+    }
+
+    draw() {
+        this.context.save();
+        let angle = Math.atan2(this.player.y, - this.y, this.player.x - this.x);
+        this.context.translate( this.x, this.y);
+        this.context.rotate(angle + Math.PI/2);
+        this.context.translate(this.x, this.y);
+        
     }
 }
